7540a9bf874c3a61f11e39e7f435a80f01d8eb09
Dave Van <57775817+meowmix1337@users.noreply.github.com>
2024-08-23 23:02:19 -0400

    Add redis

---
+++ cache/redis.go
@@ -0,0 +1,69 @@
+package cache
+
+import (
+	"context"
+	"time"
+
+	redis "github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog/log"
+)
+
+type redisCache struct {
+	client *redis.Client
+}
+
+func NewRedisCache(addr, password string, db int) *redisCache {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
+
+	return &redisCache{
+		client: client,
+	}
+}
+
+// Get retrieves data given a key
+func (rc *redisCache) Get(ctx context.Context, key string) (interface{}, error) {
+	result, err := rc.client.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return nil, CacheMissErr
+	} else if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// Set adds the value for a given key
+func (rc *redisCache) Set(ctx context.Context, key string, value interface{}, ttl int) error {
+	return rc.client.Set(ctx, key, value, time.Second*time.Duration(ttl)).Err()
+}
+
+// Delete removes the item from the cache given the key
+func (rc *redisCache) Delete(ctx context.Context, key string) error {
+	_, err := rc.client.Del(ctx, key).Result()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Purge clear all items in the cache
+func (rc *redisCache) Purge(ctx context.Context) {
+	err := rc.client.FlushDB(ctx).Err()
+	if err != nil {
+		log.Err(err).Msg("failed to flush redis DB")
+	}
+}
+
+// Size returns the number of elements in the cache
+func (rc *redisCache) Size(ctx context.Context) uint64 {
+	size, err := rc.client.DBSize(ctx).Result()
+	if err != nil {
+		log.Err(err).Msg("failed to return size of redis cache")
+		return 0
+	}
+	return uint64(size)
+}
--- go.mod
+++ go.mod
@@ -3,17 +3,21 @@ module github.com/meowmix1337/go-core
 go 1.22.5
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/redis/go-redis/v9 v9.6.1
 	github.com/rs/zerolog v1.33.0
 	github.com/stretchr/testify v1.9.0
 )
 
 require (
-	github.com/DATA-DOG/go-sqlmock v1.5.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/sys v0.23.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
